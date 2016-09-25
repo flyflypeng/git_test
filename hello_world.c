f20c320b3765fad69c749f5b77e1470181231791
woshijpf <woshijpfgg@163.com>
2016-09-25 19:24:41 +0800

    add hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+/*
+* @Author: woshijpf
+* @Date:   2016-09-25 18:52:11
+* @Last Modified by:   woshijpf
+* @Last Modified time: 2016-09-25 18:52:38
+*/
+
+#include <stdio.h>
+
+int main() {
+
+	printf("Hello world!\n");
+
+	return 0;
+}
