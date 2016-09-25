b6abb1c437d8ff33c02dfdcbf90a396dabdc3446
woshijpf <woshijpfgg@163.com>
2016-09-25 20:54:43 +0800

    add haha.cpp file

---
+++ haha.cpp
@@ -0,0 +1,17 @@
+/*
+* @Author: woshijpf
+* @Date:   2016-09-25 20:53:35
+* @Last Modified by:   woshijpf
+* @Last Modified time: 2016-09-25 20:53:58
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "This is a cpp program" << endl;
+
+	return 0;
+}
